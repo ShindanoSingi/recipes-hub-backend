a0a10cd7980a1b0556549d72b5cf4b915d9a95be
Shindano Singi <shindanosingi@sekur.com>
2023-09-13 23:51:06 -0400

    Commented

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -1,40 +1,40 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const recipeSchema = new mongoose.Schema({
-//     title: {
-//         type: String,
-//         required: [true, 'Title is required'],
-//     },
-//     imageUrl: {
-//         type: String,
-//     },
-//     description: {
-//         type: String,
-//     },
-//     ingredients: {
-//         type: String,
-//     },
-//     instructions: {
-//         type: String,
-//     },
-//     category: {
-//         type: String,
-//     },
-//     likes: {
-//         type: Number,
-//         default: 0,
-//     },
-//     dislikes: {
-//         type: Number,
-//         default: 0,
-//     },
-//     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
-//     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-// },
-// {
-//     timestamps: true,
-// });
+const recipeSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Title is required'],
+    },
+    imageUrl: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    ingredients: {
+        type: String,
+    },
+    instructions: {
+        type: String,
+    },
+    category: {
+        type: String,
+    },
+    likes: {
+        type: Number,
+        default: 0,
+    },
+    dislikes: {
+        type: Number,
+        default: 0,
+    },
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+},
+{
+    timestamps: true,
+});
 
-// const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model('Recipe', recipeSchema);
 
-// module.exports = Recipe;
+module.exports = Recipe;
